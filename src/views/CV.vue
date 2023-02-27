7d5082d65986b89c5c948346bb743206c5afb88d
Alnaive <astolo86@gmail.com>
2023-02-28 01:49:10 +0800

    update c

--- src/views/CV.vue
+++ src/views/CV.vue
@@ -36,7 +36,7 @@
                     </div>
                     <div class="flex items-center space-x-2">
                         <Icon icon="mdi:linkedin" width="32" height="32"/>
-                        <h1><a href="www.linkedin.com/in/fery-perdana">Fery Perdana</a></h1>
+                        <h1><a href="https://linkedin.com/in/fery-perdana">Fery Perdana</a></h1>
                     </div>
                 </div>
                 <h1 class="flex mt-5 shadow-lg rounded-lg dark:bg-indigo-500 p-2"><Icon icon="bi:database-gear" width="32" height="32" /> <span class="ml-2 text-lg ">Keahlian</span> </h1>
